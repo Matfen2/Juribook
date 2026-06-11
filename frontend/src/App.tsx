071e16db9f594d7891bfce264af0d977e39b3fa9
Mathieu FENOUIL <matfen3.05@gmail.com>
2026-06-11 20:01:16 +0200

    feat(frontend): création du projet react/ts (Sprint 0.3)
    - Création du projet react/ts via npm create vite@latest . -- --template react/ts
    - Ajout de la dépendance tailwindcss via npm install -D tailwindcss @tailwindcss/vite
    - Création de la page d'accueil vide via App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <section className="text-center items-center justify-center bg-cyan-800 text-amber-50 min-h-screen">
+      <h2>Bienvenue sur le site Juribook</h2>
+    </section>
+  )
+}
+
+export default App
